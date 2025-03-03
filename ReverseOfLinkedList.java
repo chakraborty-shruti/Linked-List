4acc11d79698bf8a79f91a03c74f9a7ed14fcea9
SHRUTI CHAKRABORTY <90564186+chakraborty-shruti@users.noreply.github.com>
2025-03-03 19:58:39 +0530

    ReverseOfLinkedList.java

---
+++ ReverseOfLinkedList.java
@@ -0,0 +1,49 @@
+class Node{
+    int data;
+    Node next;
+
+    Node(int k){
+        data = k;
+        next = null;
+    }
+}
+
+class ReverseOfLinkedList{
+
+    static Node reverseList(Node head){
+        Node curr = head;
+        Node next = null;
+        Node prev = null;
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    static void printList(Node head){
+        Node curr = head;
+        while(curr != null){
+            System.out.println(curr.data+" ");
+            curr = curr.next;
+        }
+    }
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+
+        System.out.println("Print the list of linked List");
+        printList(head);
+
+        head = reverseList(head);
+
+        System.out.println("Revsesed Linked List");
+        printList(head);
+
+    }
+}
